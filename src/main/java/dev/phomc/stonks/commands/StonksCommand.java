4ab799a8853348d0f35699fc072c0dc295a18460
nahkd123 <nahkd123@gmail.com>
2023-03-05 21:43:43 +0700

    Add '/market myoffers' command

--- src/main/java/dev/phomc/stonks/commands/StonksCommand.java
+++ src/main/java/dev/phomc/stonks/commands/StonksCommand.java
@@ -4,18 +4,27 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import dev.phomc.stonks.markets.Market;
 import dev.phomc.stonks.ui.menus.MainMenu;
+import dev.phomc.stonks.ui.menus.offers.OffersListMenu;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.server.level.ServerPlayer;
 
 public class StonksCommand {
 	public static LiteralArgumentBuilder<CommandSourceStack> market() {
-		return Commands.literal("market").requires(v -> v.hasPermission(Commands.LEVEL_ALL)).executes(ctx -> {
-			ServerPlayer player = ctx.getSource().getPlayerOrException();
-			Market market = Market.from(player);
-			MainMenu menu = new MainMenu(market, null, player);
-			menu.open();
-			return 1;
-		});
+		return Commands.literal("market").requires(v -> v.hasPermission(Commands.LEVEL_ALL))
+				.then(Commands.literal("myoffers").executes(ctx -> {
+					ServerPlayer player = ctx.getSource().getPlayerOrException();
+					Market market = Market.from(player);
+					OffersListMenu menu = new OffersListMenu(market, null, player);
+					menu.open();
+					return 1;
+				}))
+				.executes(ctx -> {
+					ServerPlayer player = ctx.getSource().getPlayerOrException();
+					Market market = Market.from(player);
+					MainMenu menu = new MainMenu(market, null, player);
+					menu.open();
+					return 1;
+				});
 	}
 }
